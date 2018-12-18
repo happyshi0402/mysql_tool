b256e4ce176b232e557fd30efa78398e50fcf5da
wangshifeng <785707939@qq.com>
2018-12-18 17:24:54 +0800

    mysql tool

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+setup(
+    name='Mysql-Tool',
+    version='0.0.1',
+    description='Access use mysql for MysqlDb.',
+    url='https://github.com/happyshi0402/mysql_tool.git',
+        
+    author='Wang Shifeng',
+    author_email='wsf121116@163.com',
+    license='BSD-3',
+
+    install_requires=[
+    ],
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: Chinese',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+    ],
+    packages=['mysql_tool'],
+    
+)
