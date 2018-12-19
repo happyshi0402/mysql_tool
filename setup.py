465bf63b719058a6f2d5e373226bd940224649dd
wangshifeng <785707939@qq.com>
2018-12-19 09:10:20 +0800

    添加License

--- setup.py
+++ setup.py
@@ -3,23 +3,22 @@ from distutils.core import setup
 setup(
     name='MySQL-Tool',
     version='0.0.1',
-    description='MysqlDb工具的进一步封装调用，使调用方法更简单.',
+    description='MysqlDb module use.',
     url='https://github.com/happyshi0402/mysql_tool.git',
         
     author='Wang Shifeng',
     author_email='wsf121116@163.com',
     license='MIT',
 
-    install_requires=[
-    ],
-
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
+        'Environment :: MysqlDb Environment',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: MIT License',
         'Natural Language :: Chinese',
+        'Topic :: Software Development :: Libraries :: Python Modules',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
     ],
     packages=['mysql_tool'],
     
